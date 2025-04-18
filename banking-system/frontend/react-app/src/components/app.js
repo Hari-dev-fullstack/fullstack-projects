2f1c479448ba63736aba1f381ebfdad24620abf4
Hari-dev-fullstack <sriharibellamkonda01@gmail.com>
2025-04-17 21:14:29 -0400

    Create app.js

---
+++ banking-system/frontend/react-app/src/components/app.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Dashboard from './components/Dashboard';
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to Banking System</h1>
+      <Dashboard />
+    </div>
+  );
+}
+
+export default App;
