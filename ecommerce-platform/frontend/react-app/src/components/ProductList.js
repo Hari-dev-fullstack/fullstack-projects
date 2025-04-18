19bf87070e5fed5de1816162ec764a2d9f6adf42
Hari-dev-fullstack <sriharibellamkonda01@gmail.com>
2025-04-17 21:17:22 -0400

    Update ProductList.js

--- ecommerce-platform/frontend/react-app/src/components/ProductList.js
+++ ecommerce-platform/frontend/react-app/src/components/ProductList.js
@@ -1,1 +1,13 @@
 // ProductList Component Placeholder
+import React from 'react';
+
+function ProductList() {
+  return (
+    <div>
+      <h2>All Products</h2>
+      {/* Product cards will go here */}
+    </div>
+  );
+}
+
+export default ProductList;
